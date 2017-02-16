e6953cb18306989d394f7d59816aae9c08dc5f3a
Geo Perez <geovanni.perez@gmail.com>
2017-02-16 16:44:31 -0600

    Boilerplate

---
+++ Templates/UnosquareCompositeWorker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Unosquare.Swan;
+using Unosquare.Swan.Abstractions;
+
+namespace UnosquareCompositeWorker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            $"API URL: {SettingsProvider<AppSettings>.Instance.Global.ApiUrl}".Info();
+        }
+    }
+}
